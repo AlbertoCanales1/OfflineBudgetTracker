4a7c3f17541f935b8146c7d11e9277d35eac9b42
AlbertoCanales1 <albertocanales1@outlook.com>
2021-10-03 15:46:41 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const compression = require("compression");
 
 
 
-const PORT = process.env.PORT || 3000;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
+
+
+
+// const PORT = process.env.PORT || 3000;
 
 // const PORT = 3000
 
@@ -24,12 +36,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+
 
 // routes
 app.use(require("./routes/api.js"));
