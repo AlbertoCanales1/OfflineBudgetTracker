e87eb066c34541741c13036f58c47957aecfe39f
AlbertoCanales1 <albertocanales1@outlook.com>
2021-10-02 19:57:36 -0500

    fixed server issue

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const {ApolloServer} = require("apollo-server-express")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+
+let server
+async function startServer() {
+
+  server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware,
+
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startServer();
 
 app.use(logger("dev"));
 
